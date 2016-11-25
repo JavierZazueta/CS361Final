99b944c15f87c5ece6d00857a8e129ab300a259d
JavierZazueta <javierzazueta23@gmail.com>
2016-11-25 10:28:26 -0700

    Naive works. Both the pattern you are looking for and where it is being searched should be entered as strings

--- src/NaiveSearch.java
+++ src/NaiveSearch.java
@@ -5,59 +5,82 @@ import java.io.IOException;
 
 public class NaiveSearch
 {
-  public static void main(String[] args)
+  static int instances = 0;
+
+  static void countInstances(String pattern, String file)
   {
-    int testIndex = 0;//will be used to know how far we were able to match from test
-    int instances = 0;
-    String test = args[1];//the string that we are looking for
-    BufferedReader in;
-    try
+    int j = 0;
+    for(int i = 0; i < file.length(); i++)
     {
-      in = new BufferedReader(new FileReader(args[0]));
-
-      int i = 0;
-      int c;
-      while ((c = in.read()) != -1)//while we have not read a null char
+      if (file.charAt(i) == pattern.charAt(j)) j++;
+      else if(j > 0)
       {
-        if(c == (int)test.charAt(i))
-        {
-          i++;
-          testIndex++;
-          if(i == test.length())
-          {
-            instances++;
-            i = 0;
-            testIndex = 1;
-          }
-        }
-        //we cannot look past what we have read, because we could potentially miss the match
-        else if (testIndex > 0)
-        {
-          i = 0;
-          for(int j = 1; j < testIndex; j++)
-          {
-            if(test.charAt(i) == test.charAt(j))
-            {
-              i++;
-            }
-            else i = 0;
-          }
-        }
-        //if there is no match and we didn't find any matches previously
-        else
-        {
-          testIndex = 0;
-          i = 0;
-        }
+        i -= (j-1);
+        j = 0;
       }
-      System.out.println("There are " + instances + " instances of " + test + " in the file");
-
+      if(j == pattern.length())
+      {
+        i -= (j-1);
+        j = 0;
+        instances++;
+      }
     }
-    catch (IOException e)
-    {
-      String error = "IOException: "+e.getMessage();
-      JOptionPane.showMessageDialog(null, error, "Warning", JOptionPane.WARNING_MESSAGE);
-    }
+  }
 
+
+
+  public static void main(String[] args) throws IOException
+  {
+    String pattern = "Javier";
+    String file = "hi i am Javier, and you are?";
+
+    countInstances(pattern, file);
+
+    System.out.println("There are " + instances + " instances of " + pattern + " in the file");
+
+
+
+    //    int testIndex = 0;//will be used to know how far we were able to match from test
+//    int instances = 0;
+//    //String test = args[1];//the string that we are looking for
+//    String test = "abc";//the string that we are looking for
+//    BufferedReader in;
+//
+//      in = new BufferedReader(new FileReader(args[0]));
+//
+//      int i = 0;
+//      int c;
+//      while ((c = in.read()) != -1)//while we have not read a null char
+//      {
+//        if(c == (int)test.charAt(i))
+//        {
+//          i++;
+//          testIndex++;
+//          if(i == test.length())
+//          {
+//            instances++;
+//          }
+//        }
+//        //we cannot look past what we have read, because we could potentially miss the match
+//        else if (testIndex > 0)
+//        {
+//          i = 0;
+//          for(int j = 1; j < testIndex; j++)
+//          {
+//            if(test.charAt(i) == test.charAt(j))
+//            {
+//              i++;
+//            }
+//            else i = 0;
+//          }
+//        }
+//        //if there is no match and we didn't find any matches previously
+////        else
+////        {
+////          testIndex = 0;
+////          i = 0;
+////        }
+//
+//      }
   }
 }
